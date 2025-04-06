0f1af08f7af01bf774e20059d6619eaa423ae583
Thiago Emanuel <thiag0224@hotmail.com>
2025-04-06 12:13:16 -0300

    Add incrementador com botao usando useState

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -3,15 +3,29 @@ import { useState } from "react";
 export default function App() {
   const [input, setInput] = useState("");
   const [idade, setIdade] = useState("");
+
   const [aluno, setAluno] = useState("");
+
+  const [contador, setContador] = useState(0);
+
   function mostrarAluno() {
     setAluno(input);
     console.log(idade);
   }
+
+  function adicionar() {
+    setContador((valorAtual) => valorAtual + 1);
+  }
+
+  function diminuir() {
+    if (contador === 0) {
+      return;
+    }
+    setContador((valorAtual) => valorAtual - 1);
+  }
   return (
     <div>
       <h1>Conhecendo usaState</h1>
-
       <input
         placeholder="Digite o nome"
         value={input}
@@ -30,6 +44,12 @@ export default function App() {
       <hr />
       <h3>Bem vindo: {aluno}</h3>
       <h3>A sua idade é {idade}</h3>
+      <hr />
+      <br />
+      <h1>Contator com useState</h1>
+      <button onClick={adicionar}>+</button>
+      {contador}
+      <button onClick={diminuir}>-</button>
     </div>
   );
 }
