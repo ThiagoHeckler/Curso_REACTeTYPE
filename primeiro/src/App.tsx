d82326147132f3b865196e26e34ed38149c33d07
Thiago Emanuel <thiag0224@hotmail.com>
2025-04-06 13:59:36 -0300

    Treinando mais o useState

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,55 +1,16 @@
 import { useState } from "react";
 
 export default function App() {
-  const [input, setInput] = useState("");
-  const [idade, setIdade] = useState("");
+  const [user, setUser] = useState("Olá visitante");
 
-  const [aluno, setAluno] = useState("");
-
-  const [contador, setContador] = useState(0);
-
-  function mostrarAluno() {
-    setAluno(input);
-    console.log(idade);
-  }
-
-  function adicionar() {
-    setContador((valorAtual) => valorAtual + 1);
-  }
-
-  function diminuir() {
-    if (contador === 0) {
-      return;
-    }
-    setContador((valorAtual) => valorAtual - 1);
+  function handleLogin() {
+    setUser("Olá Thiago Emanuel");
   }
   return (
     <div>
       <h1>Conhecendo usaState</h1>
-      <input
-        placeholder="Digite o nome"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <br />
-      <br />
-      <input
-        placeholder="Digite a sua idade"
-        value={idade}
-        onChange={(e) => setIdade(e.target.value)}
-      />
-      <br />
-      <br />
-      <button onClick={mostrarAluno}> Mostrar Aluno </button>
-      <hr />
-      <h3>Bem vindo: {aluno}</h3>
-      <h3>A sua idade é {idade}</h3>
-      <hr />
-      <br />
-      <h1>Contator com useState</h1>
-      <button onClick={adicionar}>+</button>
-      {contador}
-      <button onClick={diminuir}>-</button>
+      <button onClick={handleLogin}>Entrar</button>
+      <h4>{user}</h4>
     </div>
   );
 }
