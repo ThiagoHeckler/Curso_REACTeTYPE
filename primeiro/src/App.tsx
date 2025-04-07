80b00c7cda19e3a60f9a694be52ccacb9d246045
Thiago Emanuel <thiag0224@hotmail.com>
2025-04-07 06:30:57 -0300

    Porjeto das aulas 10 e 11 criando calculadora de gasolina x alcool

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 
+interface UserProps {
+  nome: string;
+  cargo: string;
+}
+
 export default function App() {
-  const [user, setUser] = useState("Olá visitante");
+  const [user, setUser] = useState<UserProps>({
+    nome: "Visitante",
+    cargo: "",
+  });
 
   function handleLogin() {
-    setUser("Olá Thiago Emanuel");
+    setUser({
+      nome: "Sujeito Programador",
+      cargo: "Programador",
+    });
+  }
+  function handleLogout() {
+    setUser({
+      nome: "Visitante",
+      cargo: "",
+    });
   }
   return (
     <div>
       <h1>Conhecendo usaState</h1>
       <button onClick={handleLogin}>Entrar</button>
-      <h4>{user}</h4>
+      <button onClick={handleLogout}>Fazer logout</button>
+      <h4>Olá {user.nome}</h4>
+      <strong>{user.cargo}</strong>
     </div>
   );
 }
